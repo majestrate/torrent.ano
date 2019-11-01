100a3a3a0a519d79e61941bb458ca9a280a7c6fc
wipedlife <gigoga367@gmail.com>
2019-11-01 19:10:36 +0700

    scrape tests

--- src/tracker/scrape/getFile.go
+++ src/tracker/scrape/getFile.go
@@ -8,15 +8,15 @@ import(
 )
 
 
-func DownloadFile(filepath string, url string){
-	response, err = http.Get(url);
+func DownloadFile(filepath string, url string) (err error){
+	response, err := http.Get(url);
 	if err!=nil{ return err	}
 	defer response.Body.Close();
 
-	outfile, err = os.Create(config_scrape.DEFAULT_SCRAPE_FILE_PATH);
+	outfile, err := os.Create(config_scrape.DEFAULT_SCRAPE_FILE_PATH);
 	if err!=nil{ return err	}
 	defer outfile.Close();
 
-	_,err =io.Copy(outfile.response.Body)
+	_,err =io.Copy( outfile , response.Body )
 	return err;
 }
---
+++ src/tracker/scrape/scrape_test.go
@@ -0,0 +1,28 @@
+package scrape
+
+import "testing"
+import config_scrape "./config_scrape"
+import "fmt"
+
+
+func TestDownloadFile(t *testing.T){
+	err:= DownloadFile(config_scrape.DEFAULT_SCRAPE_FILE_PATH,config_scrape.DEFAULT_SCRAPE_URL);
+	if err!=nil{
+		t.Error("Err with download file")
+	}
+
+}
+
+func TestReadScrape(t *testing.T){
+	switch data:=ReadScrape().(type) {
+		case map[string]map[string]string :
+
+			fmt.Println("Its map");
+			fmt.Println(data);
+		default:
+			fmt.Println("I get error");
+			fmt.Println(data);
+
+	}
+
+}
