9232eb1061a332051fed3685e5ad7c5e776af9a9
wipedlife <gigoga367@gmail.com>
2019-11-02 12:09:19 +0700

    scrape fix map

--- src/tracker/scrape/scrape.go
+++ src/tracker/scrape/scrape.go
@@ -2,9 +2,32 @@ package scrape
 
 import (
 	"fmt"
-	b "github.com/jackpal/bencode-go"
+	b "github.com/felix/go-bencode"
+
+	"crypto/sha1"
+	"io"
 	"os"
 )
+
+func FilesConstructMap(raw interface{}) map[string]map[string]int64 {
+	tmp := raw.(map[string]interface{})
+	tmp1 := tmp["files"]
+	tmp2 := tmp1.(map[string]interface{})
+	ret := make(map[string]map[string]int64)
+
+	h := sha1.New()
+	for key, value := range tmp2 {
+		io.WriteString(h, key)
+		key = fmt.Sprintf("%x", h.Sum(nil))
+		tmp_ := value.(map[string]interface{})
+
+		ret[key] = make(map[string]int64)
+		for K, V := range tmp_ {
+			ret[key][K] = V.(int64)
+		}
+	}
+	return ret
+}
 
 func ReadScrape(file_path string) interface{} {
 	file, err := os.Open(file_path)
@@ -23,11 +46,11 @@ func ReadScrape(file_path string) interface{} {
 		fmt.Println("Scrape file is null size")
 		return err
 	}
-	data, err := b.Decode(file)
+	var tmp0 interface{}
+	tmp, err := b.Decode(file)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	return data
-
 }
--- src/tracker/scrape/tests/bencode.go
+++ src/tracker/scrape/tests/bencode.go
@@ -1,68 +1,83 @@
 package main
-import ("fmt"
+
+import (
 	"crypto/sha1"
+	"fmt"
+	b "github.com/felix/go-bencode"
 	"io"
 	"os"
-	"reflect"
-	b "github.com/felix/go-bencode"
 )
 
-func main(){
-	file,err:=os.Open("scrape")
-	if err != nil{
+func FilesConstructMap(raw interface{}) map[string]map[string]int64 {
+	tmp := raw.(map[string]interface{})
+	tmp1 := tmp["files"]
+	tmp2 := tmp1.(map[string]interface{})
+	ret := make(map[string]map[string]int64)
+
+	h := sha1.New()
+	for key, value := range tmp2 {
+		io.WriteString(h, key)
+		key = fmt.Sprintf("%x", h.Sum(nil))
+		tmp_ := value.(map[string]interface{})
+
+		ret[key] = make(map[string]int64)
+		for K, V := range tmp_ {
+			ret[key][K] = V.(int64)
+		}
+	}
+	return ret
+}
+func main() {
+	file, err := os.Open("scrape")
+	if err != nil {
 		fmt.Println("Error with open file!")
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer file.Close()
-	stat, err := file.Stat();
-	if err != nil{
+	stat, err := file.Stat()
+	if err != nil {
 		fmt.Println("Error with get size of file")
 		os.Exit(1)
 	}
 	fmt.Print("file size: ")
 	fmt.Println(stat.Size())
-//	bs:=make([]byte, stat.Size())
-//	_, err = file.Read(bs)
-//	if err!=nil{
-//		fmt.Println("Error with read of file")
-//		os.Exit(1)
-//	}
-//	fmt.Println(bs)
+	//	bs:=make([]byte, stat.Size())
+	//	_, err = file.Read(bs)
+	//	if err!=nil{
+	//		fmt.Println("Error with read of file")
+	//		os.Exit(1)
+	//	}
+	//	fmt.Println(bs)
 	fmt.Println("decode init")
 	var i interface{}
-	packet:= make ([]byte, stat.Size())
-	_,err = file.Read(packet)
-	if err != nil{
+	packet := make([]byte, stat.Size())
+	_, err = file.Read(packet)
+	if err != nil {
 		fmt.Println("Error with read file")
 		os.Exit(1)
 	}
-	i, err =b.Decode(packet)
-	if err !=nil{
+	i, err = b.Decode(packet)
+	if err != nil {
 		fmt.Println("Error with decode")
 		os.Exit(1)
 	}
+	tmp2 := FilesConstructMap(i)
 	//tmp:=tmp0["files"].(map[string]map[interface{}]map[string]int)
-	tmp:=i.(map[string]interface {})
-	tmp1:=tmp["files"]
-	tmp2:=tmp1.(map[string]interface {})
-	fmt.Println( reflect.TypeOf(tmp2) )
-	for k,v := range tmp2{
+	for k, v := range tmp2 {
+		fmt.Print(k + ": ")
 		fmt.Println(v)
-		h:=sha1.New()
-		io.WriteString(h, k)
-		fmt.Printf("%x :\n", h.Sum(nil) )
 	}
 	//tmp:= i["files"].(map[string]interface{})
 
-	m:=map[string]map[string]string {
-		"some": map[string]string {
-			"some1":"value",
+	m := map[string]map[string]string{
+		"some": map[string]string{
+			"some1": "value",
 		},
 	}
 	fmt.Println(m)
 
-	m1:=map[string]string{
+	m1 := map[string]string{
 		"Just string": "Too just string",
 	}
 
