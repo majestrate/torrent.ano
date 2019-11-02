9dfccc38d1536ecb6e3dba55babbf948dd21defc
wipedlife <gigoga367@gmail.com>
2019-11-02 13:11:47 +0700

    go fmt

--- src/tracker/scrape/scrape.go
+++ src/tracker/scrape/scrape.go
@@ -29,33 +29,33 @@ func FilesConstructMap(raw interface{}) map[string]map[string]int64 {
 	return ret
 }
 
-func ReadScrape(file_path string) ( interface{}, error) {
+func ReadScrape(file_path string) (interface{}, error) {
 	file, err := os.Open(file_path)
 	if err != nil {
 		fmt.Println(err)
-		return nil,err
+		return nil, err
 	}
 	defer file.Close()
 	//check if file is not null
 	stat, err := file.Stat()
 	if err != nil {
 		fmt.Println(err)
-		return nil,err
+		return nil, err
 	}
 	if stat.Size() == 0 {
 		fmt.Println("Scrape file is null size")
-		return nil,err
+		return nil, err
 	}
 	packet := make([]byte, stat.Size())
 	_, err = file.Read(packet)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
 	data, err := b.Decode(packet)
 	if err != nil {
 		fmt.Println(err)
-		return nil,err
+		return nil, err
 	}
 	return data, nil
 }
--- src/tracker/scrape/scrape_test.go
+++ src/tracker/scrape/scrape_test.go
@@ -14,18 +14,18 @@ func TestDownloadFile(t *testing.T) {
 
 func TestReadScrape(t *testing.T) {
 	_, err := ReadScrape(config_scrape.DEFAULT_SCRAPE_FILE_PATH)
-	if err != nil{
+	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestGetScrapeByInfoHash(t *testing.T){
-	err,mp:=GetScrapeByInfoHash(config_scrape.DEFAULT_SCRAPE_FILE_PATH, config_scrape.DEFAULT_SCRAPE_URL, "9865c87afbdf138aa3e2b88220187f38015dcfc0")
-	if err != nil{
+func TestGetScrapeByInfoHash(t *testing.T) {
+	err, mp := GetScrapeByInfoHash(config_scrape.DEFAULT_SCRAPE_FILE_PATH, config_scrape.DEFAULT_SCRAPE_URL, "9865c87afbdf138aa3e2b88220187f38015dcfc0")
+	if err != nil {
 		t.Error(err)
 	}
-	for key, value := range mp{
-		fmt.Print(key+": ")
+	for key, value := range mp {
+		fmt.Print(key + ": ")
 		fmt.Println(value)
 	}
 }
